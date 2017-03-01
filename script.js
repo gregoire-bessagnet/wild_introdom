f648184576e2e0693122b953f7707c4f9cd82cd8
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2017-03-01 17:42:56 +0100

    adding some script lines

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-var add = document.getElementById(‘add’);
-link.getAttribute(‘href’); // retourne “#”
+var newLine = document.createElement('tr');
+var rmvline =
+var fontup
+var fontdown
+
+function addline()
+function delline()
+function increase()
+function decrease()
+
+
+document.getElementById(‘add’).onclick.innerHTML =
+document.getElementById(‘del’).onclick.innerHTML =
+document.getElementById(‘more’).onclick.innerHTML =
+document.getElementById(‘less’).onclick.innerHTML =
+
+
+
+function(){
+document.createElement('tr')
+};
+
+
+window.onload = fonction_du_fichier;
+
+addEventListener(document.getElementById(‘add’), "click", addline);
+addEventListener(document.getElementById(‘del’), "click", addline);
+addEventListener(document.getElementById(‘more’), "click", addline);
+addEventListener(document.getElementById(‘less’), "click", addline);
+
