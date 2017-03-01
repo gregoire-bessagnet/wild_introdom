6b5b033673cf731506210c2c676ad049cc173e24
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2017-03-01 17:48:07 +0100

    create functions to add and delete rows

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-var newLine = document.createElement('tr');
-var rmvline =
-var fontup
-var fontdown
+function addRow() {
 
-function addline()
-function delline()
-function increase()
-function decrease()
+var table = document.getElementById("table");
+var row = table.insertRow(0);
+var cell1 = row.insertCell(0);
+cell1.innerHTML = "NEW CELL1";
 
+}
 
-document.getElementById(‘add’).onclick.innerHTML =
-document.getElementById(‘del’).onclick.innerHTML =
-document.getElementById(‘more’).onclick.innerHTML =
-document.getElementById(‘less’).onclick.innerHTML =
-
-
-
-function(){
-document.createElement('tr')
-};
-
-
-window.onload = fonction_du_fichier;
-
-addEventListener(document.getElementById(‘add’), "click", addline);
-addEventListener(document.getElementById(‘del’), "click", addline);
-addEventListener(document.getElementById(‘more’), "click", addline);
-addEventListener(document.getElementById(‘less’), "click", addline);
-
+function deleterow() {
+    document.getElementById("table").deleteRow(0);
+}
